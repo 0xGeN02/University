a44ded8455f2be78d2bde02df53740af208b6536
0xGeN02 <manuelmateodgl02@gmail.com>
2024-02-28 13:59:42 +0100

    Adding Bayesian

--- 2/OptimizacionMatematica/s03.m
+++ 2/OptimizacionMatematica/s03.m
@@ -1,25 +1,22 @@
 %Préambulo
-%Vector de coste
-c=[6 6];
-%Matriz de coeficientes de las restricciones
-A=[10 20; 30 20; 15 70];
-%Vector de restricciones
-b=transpose([800 1600 1800]);
-%Cota inferior de las variables
-lb=[0 0];
-%Cota superior de las variables
-ub=[];
-%Tipo de restricciones
-ctype="LLL";
-%Tipo de variables de decisión
-vartype="CC";
-%Sentido de la optimización (minimización)
-s=1;
+c = [4; -2; 7; -1]; %Vector de coste
+A = [1, 0, 5, 0; 1, 1, -1, 0; 1, -5, 0, 0; 0, 1, 1, -1]; %Matriz de coeficientes
+b = [10; 1; 0; 2]; %Vector de restricciones
+lb = zeros(4, 1); %Cota inferior de las variables
+ub = []; %Cota superior de las variables
+ctype = "<="; %Tipo de restricciones
+vartype = "C"; %Tipo de variables de decisión
+s = "max"; %Sentido de la optimización (maximización)
+
 %Parámetros de computación
-param.msglev=3;
-param.itlim=10;
+param.msglev = 3;
+param.itlim = 10;
+
 %Optimización
-%xmin punto donde se alcanza el valor mínimo de la función objetivo fmin
-[xmin,fmin]=glpk(c,A,b,lb,ub,ctype,vartype,s,param);
-disp(xmin)
-disp(fmin)
+[xmin, fmin] = glpk(c, A, b, lb, ub, ctype, vartype, s, param);
+
+%Mostrar resultados
+disp("Solución óptima (x1, x2, x3, x4):");
+disp(xmin);
+disp("Valor máximo de la función objetivo:");
+disp(fmin);
