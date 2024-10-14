47df7ddc75469e73bf75aceb6a2f6ca7eadfa040
0xGeN02 <manuelmateodgl02@gmail.com>
2024-10-14 18:43:49 +0200

    practica evaluada 2

---
+++ 3/AutomatizacionRobotizacion/Arduino/lib/Practica2/IoTP02E04.h
@@ -0,0 +1,26 @@
+#ifndef IoT_P02_E04_H
+#define IoT_P02_E04_H
+
+#include <Arduino.h>
+
+
+// Vars and globals
+extern const int P_Led1;
+extern const int P_Led2;
+extern const int P_Led3;
+extern const int P_Led4;
+extern const int P_Led5;
+extern const int P_Pulsador1;
+extern const int P_Pulsador2;
+
+extern const int numLedsEncendidos;
+extern const bool estadoAnteriorPulsador1;
+extern const bool estadoAnteriorPulsador2;
+
+// Functions
+void setup_P02E04(){}
+void loop_P02E04(){}
+void actualizacionleds(){}
+void apadaTodosLeds(){}
+
+#endif //IoT_P02_E04_H
