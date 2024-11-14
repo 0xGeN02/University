66295ac46fd690d80fc17c7ed19cd39b07534c3e
0xGeN02 <manuelmateodgl02@gmail.com>
2024-11-14 17:46:16 +0100

    aaa

--- 3/AutomatizacionRobotizacion/Niryo/ned2/main.py
+++ 3/AutomatizacionRobotizacion/Niryo/ned2/main.py
@@ -1,3 +1,7 @@
+"""
+ Practica 0: Dibujar figuras en el papel DinA3
+"""
+from typing import List, Literal
 import pyniryo2 as pn2
 
 # Conectivity Constants
@@ -9,14 +13,17 @@ HOTSPOT_NAME: str = "ea-40f-1a0"
 HOTSPOT_PASSWORD: str = "niryorobot"
 
 # World Constants
-DIN_A3_WIDTH = 0.42
-DIN_A3_HEIGHT = 0.297
-ROBOT_X = None
-ROBOT_Y = None
-ROBOT_Z = None
-ROBOT_ROLL = None
-ROBOT_PITCH = None
-ROBOT_YAW = None
+DIN_A3_WIDTH = 0.043
+DIN_A3_HEIGHT = -0.2
+
+# Robot Constants
+ROBOT_X = -3.0
+ROBOT_Y = 0.609
+ROBOT_Z = -0.728
+ROBOT_ROLL = -1.213
+ROBOT_PITCH = 1.558
+ROBOT_YAW = 0.061
+INITIAL_COORDENATE = [ROBOT_X, ROBOT_Y, ROBOT_Z, ROBOT_ROLL, ROBOT_PITCH, ROBOT_YAW]
 
 class World():
     """
@@ -50,38 +57,17 @@ class World():
         """
         return [(0, 0), (0, self.height), (self.width, self.height), (self.width, 0)]
 
-class Robot():
+class RobotDrawer(pn2.NiryoRobot):
     """
-    Definicion del robot
+    Definicion del robot que es capaz de dibujar
     """
-    def __init__(self, robot: pn2.NiryoRobot = None):
-        self.robot = robot
+    def __init__(self, hotspot_address: str, port: int):
+        super().__init__(hotspot_address, port)
+        self.arm.calibrate_auto()
+        self.arm.go_to_sleep()
+        self.tool.update_tool()
 
-    def get_robot(self):
-        """
-        Devuelve el robot
-        """
-        return self.robot
-
-    def connect_hotspot(self, hotspot_address: str, port: int):
-        """
-        Conectarse al hotspot
-        """
-
-        self.robot = pn2.NiryoRobot(hotspot_address, port)
-        self.robot.arm.calibrate_auto()
-        self.robot.arm.go_to_sleep()
-        self.robot.tool.update_tool()
-
-    def robot_to_world(self, x:float, y: float, z: float, roll: float, pitch: float, yaw: float):
-        """
-        Asociar coordenadas del robot con el mundo
-        """
-        if(x | y | z | roll | pitch | yaw) is None:
-            return "Error: No se han definido las coordenadas del robot"
-        return (x, y, z, roll, pitch, yaw)
-
-    def draw_square(self, center: list[x: float, y:float, z: float], side: float):
+    def draw_square(self, center: List[float], side: float, height: float = 0.1):
         """
         Dibujar un cuadrado en el mundo
         @param center_x: Centro del cuadrado en el eje x
@@ -89,17 +75,22 @@ class Robot():
         @param side: Lado del cuadrado
         @param n: Altura z del robot respecto al folio
         """
-        half_center = side / 2
-        corners = [(center.x - half_center, center.y - half_center),
-                   (center.x - half_center, center.y + half_center),
-                   (center.x + half_center, center.y + half_center),
-                   (center.x + half_center, center.y - half_center)]
-        for point in corners:
-            self.robot.arm.move_joints([point[0], point[1], point[2], 0, 0, 0])
+        half_side = side / 2
+        corners = [
+            (center[0] - half_side, center[1] - half_side),
+            (center[0] - half_side, center[1] + half_side),
+            (center[0] + half_side, center[1] + half_side),
+            (center[0] + half_side, center[1] - half_side)
+        ]
 
-    def draw_triangle(self, center: list [x:float, y: float, z: float | None], type: str ='equilatero'):
+        for point in corners:
+            if point == corners[0]:
+                self.arm.pose([point[0], point[1], -height, 0.0, 0.0, 0.0])
+            self.arm.pose([point[0], point[1], 0.0, 0.0, 0.0, 0.0])
+
+    def draw_triangle(self, center: List[float], type: Literal['equilatero', 'escaleno', 'isosceles']):
         """
-        
+        Dibujar un triángulo
         """
         if type in ['equilatero', 'escaleno', 'isosceles']:
             if type == 'equilatero':
@@ -109,34 +100,43 @@ class Robot():
             else:
                 pass
         else:
-            'Triangle TYPE not defined'
+            print('Triangle TYPE not defined')
 
-    def draw_circle(self, center: list[x: float, y:float, z:float | None], radius: float):
+    def draw_circle(self, center: List[float], radius: float):
         """
-        
+        Dibujar un circulo dado el radio
         """
         pass         
-
 
 def main():
     """
     Main function
     """
-    # Create the world
-    world = World(DIN_A3_WIDTH, DIN_A3_HEIGHT)
+    # Crear el mundo
+    paper = World(DIN_A3_WIDTH, DIN_A3_HEIGHT)
 
-    # Create the robot
-    robot = Robot()
+    # Conectar al robot
+    robot = RobotDrawer(HOTSPOT_ADDRESS, PORT)
 
-    # Connect to the hotspot
-    robot.connect_hotspot(HOTSPOT_ADDRESS, PORT)
-
-    if robot.get_robot() is None:
+    # Verificar la conexión del robot
+    if robot is None:
         print("Robot connection failed.")
     else:
         print("Robot connected successfully.")
 
+    # Calcular el centro del mundo
+    paper_center = paper.get_center()
 
+    #Subimos en el eje z el robot para no pintar el papel mientras lo llevamos al primer vertice
+    height = 0,1
+    INITIAL_COORDENATE[3] = INITIAL_COORDENATE[3] + height
+    robot.arm.move_joints(INITIAL_COORDENATE)
+
+    #Asociamos el centro del robot con las coordenadas del robot
+    robot_center = [paper_center[0]+ ROBOT_X, paper_center[1] + ROBOT_Y]
+
+    # Dibujar un cuadrado
+    robot.draw_square([robot_center[0], robot_center[1]], 0.1)
 
 if __name__ == "__main__":
     main()
