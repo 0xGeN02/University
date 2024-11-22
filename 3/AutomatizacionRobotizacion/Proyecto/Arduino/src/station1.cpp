3c1c9eb50e78684843b51145083629c1dec141cf
0xGeN02 <manuelmateodgl02@gmail.com>
2024-11-22 16:20:05 +0100

    add start.zsh

--- 3/AutomatizacionRobotizacion/Proyecto/Arduino/src/station1.cpp
+++ 3/AutomatizacionRobotizacion/Proyecto/Arduino/src/station1.cpp
@@ -1,10 +1,22 @@
 #include <Arduino.h>
 #include "station1.h"
 
+int number = 1;
+
 void setup() {
+    // Initialize hardware serial
+    Serial.begin(9600);
+    
+    Serial.println("Station 1 running...");
+    
     setupStation1();
 }
 
 void loop() {
+    // Send number to master
+    Serial.println(number);
+    number++;
+    
     loopStation1();
+    delay(1000); // Delay to avoid flooding the serial communication
 }
